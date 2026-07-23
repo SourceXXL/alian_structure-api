171daf69e680de87a5e82726c25639cc95217305
ummarig <nafiuigee@gmail.com>
2026-07-23 17:11:43 +0100

    implemented the order routing

---
+++ src/core/file-upload/constants/queue.constants.ts
@@ -0,0 +1,6 @@
+export const FILE_UPLOAD_QUEUE = 'file-upload';
+export const FILE_PROCESSING_JOB = 'process-file';
+export const VIRUS_SCAN_JOB = 'scan-for-viruses';
+export const IMAGE_PROCESSING_JOB = 'process-images';
+export const METADATA_EXTRACTION_JOB = 'extract-metadata';
+export const CLEANUP_JOB = 'cleanup-orphaned-files';
---
+++ src/core/file-upload/file-upload.module.ts
@@ -0,0 +1,65 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { MulterModule } from '@nestjs/platform-express';
+import { ScheduleModule } from '@nestjs/schedule';
+import { BullModule } from '@nestjs/bull';
+import { FileUploadService } from './services/file-upload.service';
+import { FileStorageService } from './services/file-storage.service';
+import { FileValidationService } from './services/file-validation.service';
+import { VirusScannerService } from './services/virus-scanner.service';
+import { ImageProcessingService } from './services/image-processing.service';
+import { MetadataExtractionService } from './services/metadata-extraction.service';
+import { UrlSigningService } from './services/url-signing.service';
+import { GarbageCollectionService } from './services/garbage-collection.service';
+import { UploadProgressService } from './services/upload-progress.service';
+import { FileUploadController } from './controllers/file-upload.controller';
+import { FileDownloadController } from './controllers/file-download.controller';
+import { LocalStorageBackend } from './storage-backends/local-storage.backend';
+import { S3StorageBackend } from './storage-backends/s3-storage.backend';
+import { AzureBlobStorageBackend } from './storage-backends/azure-blob-storage.backend';
+import { StorageBackendFactory } from './storage-backends/storage-backend.factory';
+import { UploadedFileEntity } from './entities/uploaded-file.entity';
+import { FileMetadataEntity } from './entities/file-metadata.entity';
+import { UploadSessionEntity } from './entities/upload-session.entity';
+import { FILE_UPLOAD_QUEUE } from './constants/queue.constants';
+
+@Module({
+  imports: [
+    ConfigModule,
+    TypeOrmModule.forFeature([
+      UploadedFileEntity,
+      FileMetadataEntity,
+      UploadSessionEntity,
+    ]),
+    MulterModule.registerAsync({
+      useFactory: () => ({
+        limits: {
+          fileSize: 100 * 1024 * 1024, // 100MB default
+        },
+      }),
+    }),
+    ScheduleModule.forRoot(),
+    BullModule.registerQueue({
+      name: FILE_UPLOAD_QUEUE,
+    }),
+  ],
+  controllers: [FileUploadController, FileDownloadController],
+  providers: [
+    FileUploadService,
+    FileStorageService,
+    FileValidationService,
+    VirusScannerService,
+    ImageProcessingService,
+    MetadataExtractionService,
+    UrlSigningService,
+    GarbageCollectionService,
+    UploadProgressService,
+    LocalStorageBackend,
+    S3StorageBackend,
+    AzureBlobStorageBackend,
+    StorageBackendFactory,
+  ],
+  exports: [FileUploadService, FileStorageService],
+})
+export class FileUploadModule {}
